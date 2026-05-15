ee0999d6e9b6e95b211a0a268b8ba224d339cd77
Yan-sys37 <y147265615@gmail.com>
2026-05-15 20:00:57 +0800

    Add files via upload

---
+++ iris-ml-project/train.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import joblib
+
+data = load_iris()
+X, y = data.data, data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
+joblib.dump(model, "model.joblib")
+print("模型训练完成并保存")
